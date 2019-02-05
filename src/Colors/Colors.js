0015ee5d708c084d932f0603b3ab8c57f1f6686f
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-02-04 18:13:19 -0600

    changed to HuePicker

--- src/Colors/Colors.js
+++ src/Colors/Colors.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { ChromePicker } from 'react-color';
+import { HuePicker } from 'react-color';
 
 const Colors = ({ color1, handleChange1, color2, handleChange2 }) => {
     return (
         <div>
             <h2>Pick Your Colors</h2>
-            <ChromePicker color={color1} onChangeComplete={handleChange1} />
-            <ChromePicker color={color2} onChangeComplete={handleChange2} />
+            <HuePicker color={color1} onChange={handleChange1} />
+            <br/>
+            <HuePicker color={color2} onChange={handleChange2} />
         </div>
     )
 }
