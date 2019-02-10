7ad5432ae3f631560d8ffd7c01c5823875c0ee50
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-02-10 16:29:53 -0600

    Updates

---
+++ precache-manifest.8c63c067aa7b26f34a222f976a0e1f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3126e6346ace806516ed",
+    "url": "/colorgenerator/static/js/runtime~main.3126e634.js"
+  },
+  {
+    "revision": "41739c5c9f443c967e2e",
+    "url": "/colorgenerator/static/js/main.41739c5c.chunk.js"
+  },
+  {
+    "revision": "579f9956338fbf29449a",
+    "url": "/colorgenerator/static/js/1.579f9956.chunk.js"
+  },
+  {
+    "revision": "41739c5c9f443c967e2e",
+    "url": "/colorgenerator/static/css/main.72d08c5d.chunk.css"
+  },
+  {
+    "revision": "579f9956338fbf29449a",
+    "url": "/colorgenerator/static/css/1.56cb7081.chunk.css"
+  },
+  {
+    "revision": "8b95c401ddaf5d7bc6679a658950dba4",
+    "url": "/colorgenerator/index.html"
+  }
+];
