acb91527b2e80f92e34d578d9e64e4b3fc23811e
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-02-10 17:47:03 -0600

    Updates

--- precache-manifest.638f732c97efbd5409a3995276e4016a.js
+++ precache-manifest.638f732c97efbd5409a3995276e4016a.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/colorgenerator/static/js/runtime~main.3126e634.js"
   },
   {
-    "revision": "542740ae3aa6025ab1ed",
-    "url": "/colorgenerator/static/js/main.542740ae.chunk.js"
+    "revision": "4d86deec0161ee45bf60",
+    "url": "/colorgenerator/static/js/main.4d86deec.chunk.js"
   },
   {
     "revision": "579f9956338fbf29449a",
     "url": "/colorgenerator/static/js/1.579f9956.chunk.js"
   },
   {
-    "revision": "542740ae3aa6025ab1ed",
-    "url": "/colorgenerator/static/css/main.a9399ca1.chunk.css"
+    "revision": "4d86deec0161ee45bf60",
+    "url": "/colorgenerator/static/css/main.c44e07a9.chunk.css"
   },
   {
     "revision": "579f9956338fbf29449a",
     "url": "/colorgenerator/static/css/1.56cb7081.chunk.css"
   },
   {
-    "revision": "1c3c910a3327a8ee4d2d143966cba86f",
+    "revision": "0e22213ad1de1ee4b57304cfbe7a9e4f",
     "url": "/colorgenerator/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/colorgenerator/precache-manifest.cfde93ec79965c3c737f436c9ac5a03a.js"
+  "/colorgenerator/precache-manifest.638f732c97efbd5409a3995276e4016a.js"
 );
 
 workbox.clientsClaim();
