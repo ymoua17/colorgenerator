460267042529ce282d835d2273c1f3af80b295db
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-02-13 20:19:09 -0600

    added space

--- src/Components/Code/Code.js
+++ src/Components/Code/Code.js
@@ -4,7 +4,7 @@ class Code extends React.Component {
     render() {
         return (
             <div className="pv4">
-                <span className="f2 fw6">Code:</span><span className="fw5 f4">{this.props.code}</span>
+                <span className="f2 fw6">Code: </span><span className="fw5 f4">{this.props.code}</span>
             </div>
         )
     }
