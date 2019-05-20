e2c686a813d5eb3080b923e33c75321e5ca6801e
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-05-19 20:00:17 -0500

    Updates

---
+++ precache-manifest.ac82d9a73c5d07fbacc8b73910487d9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd975e3200778bda4f16e23fd28a389",
+    "url": "/colorgenerator/index.html"
+  },
+  {
+    "revision": "f04a390f3c553aa9525a",
+    "url": "/colorgenerator/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b9c80b9bc57efd7ccb6d",
+    "url": "/colorgenerator/static/css/main.9b8e0465.chunk.css"
+  },
+  {
+    "revision": "f04a390f3c553aa9525a",
+    "url": "/colorgenerator/static/js/2.f54dcf9e.chunk.js"
+  },
+  {
+    "revision": "b9c80b9bc57efd7ccb6d",
+    "url": "/colorgenerator/static/js/main.85ff8e5d.chunk.js"
+  },
+  {
+    "revision": "d3db9cb6dd493536799d",
+    "url": "/colorgenerator/static/js/runtime~main.331c21f8.js"
+  },
+  {
+    "revision": "884fa3b0857b57253ee046ee923fd001",
+    "url": "/colorgenerator/static/media/colorwheel.884fa3b0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/colorgenerator/precache-manifest.7061f1f81073f2dc8e73e701bc403c00.js"
+  "/colorgenerator/precache-manifest.ac82d9a73c5d07fbacc8b73910487d9b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/colorgenerator/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/colorgenerator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
